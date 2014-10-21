dce1f92ef3e3c2fe037b29f628395a19943824bc
wern <ancheta.wern@gmail.com>
2014-10-21 17:10:33 +0800

    remove unnecessary code

--- portfolio.rb
+++ portfolio.rb
@@ -21,62 +21,27 @@ module Jekyll
       portfolio_dir_path = context.registers[:site].config['portfolio_path']
       portfolio_dir = Dir.new portfolio_dir_path
 
-      if(@project_folder == "")
+      Dir.foreach(portfolio_dir_path + "/" + @project_folder) do |screenshot|
+        if(screenshot != "." && screenshot != "..")
+          
+          link = portfolio_root + "/" + @project_folder
+          img = portfolio_img_root + "/" + @project_folder + "/" + screenshot
+          title = File.basename(screenshot, File.extname(screenshot))
 
-          portfolio_dir.each do |project|
-            if(project != "." && project != "..")
-              projects.push(project)
+          real_title = title.sub("small-", "")
+          big_img = img.sub("small-", "")
 
-            end
-          end
-
-          projects.each do |project_name|
-            Dir.foreach(portfolio_dir_path + "/" + project_name) do |screenshot|
-              if(screenshot != "." && screenshot != "..")
-                
-                link = portfolio_root + "/" + project_name
-                img = portfolio_img_root + "/" + project_name + "/" + screenshot
-                title = File.basename(screenshot, File.extname(screenshot))
-                
-                if(screenshot.index "main")
-
-                  content += '<div class="gallery-item">'
-                  content += '<a rel="gallery1" title="' + title + '" href="' + img + '" class="fancybox"><img src="' + img + '"></a>'
-                  content += '<h3><a href="' + link + '">' + project_name + '</a></h3>'
-                  content += '</div>'
-                end
-              end
-            end
-          end  
-
-      else
-
-        Dir.foreach(portfolio_dir_path + "/" + @project_folder) do |screenshot|
-          if(screenshot != "." && screenshot != "..")
+          if(title.index "small-")
             
-            link = portfolio_root + "/" + @project_folder
-            img = portfolio_img_root + "/" + @project_folder + "/" + screenshot
-            title = File.basename(screenshot, File.extname(screenshot))
 
-            real_title = title.sub("small-", "")
-            big_img = img.sub("small-", "")
+            content += '<div class="gallery-item">'
+            content += '<a rel="gallery1" title="' + real_title + '" href="' + big_img + '" class="fancybox"><img src="' + img + '"></a>'
+            content += '</div>'
 
-            if(title.index "small-")
-              
-
-              content += '<div class="gallery-item">'
-              content += '<a rel="gallery1" title="' + real_title + '" href="' + big_img + '" class="fancybox"><img src="' + img + '"></a>'
-              content += '</div>'
-
-            end
-            
           end
+          
         end
-
-        
       end
-      
-      
 
       return content
       
